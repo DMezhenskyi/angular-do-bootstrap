a4776d7f94cb795862cb9e4c564ec277f975b391
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2022-08-15 13:24:01 +0200

    implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
-import { NgModule } from '@angular/core';
+import { ApplicationRef, DoBootstrap, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './pro/app.component';
+import { AppComponent as ProAppComponent } from './pro/app.component';
 import { BasicComponent } from './basic/basic.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    ProAppComponent,
     BasicComponent
   ],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule],
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+
+  constructor(private http: HttpClient) {}
+
+  ngDoBootstrap(appRef: ApplicationRef) {
+    this.http.get<{ active: boolean }>(`http://localhost:5001/decoded-license-server/us-central1/checkEU001`).subscribe(
+      (data) => {
+        const APP = data.active ? ProAppComponent : BasicComponent;
+        appRef.bootstrap(APP, document.getElementById('app'))
+      }
+    )
+  }
+}
